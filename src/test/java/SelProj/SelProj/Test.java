531d5ccd65c38ecb6e2906fa1cfeec2e7939072b
AC70068 <AC70068@IND-5CG7074TVZ.ctl.intranet>
2021-12-16 22:57:21 +0530

    commit 10:57 12/16

--- src/test/java/SelProj/SelProj/Test.java
+++ src/test/java/SelProj/SelProj/Test.java
@@ -1,9 +1,35 @@
 package SelProj.SelProj;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+
 public class Test {
 
-	public static void main(String[] args) {
-		System.out.println("Lumen");
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\AC70068\\Downloads\\chromedriver_\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get("https://www.lumen.com/en-us/home.html");
+		Thread.sleep(5000);
+		System.out.println(driver.getTitle());
+
+
+		Actions action = new Actions(driver);
+		action.moveToElement(driver.findElement(By.xpath("//span[@class='tier-link'][contains(.,'SOLUTIONS')]")))
+		.moveToElement(driver.findElement(By.xpath("//span[@class='tier-link'][contains(.,'By Industry')]")))
+		.click(driver.findElement(By.xpath("(//a[contains(@class,'tier-link ')])[23]"))).build().perform();
+
+		Thread.sleep(5000);
+		System.out.println(driver.getTitle());
+
+		driver.close();
+		driver.quit();
 	}
 
 }
